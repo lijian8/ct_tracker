6946ce048115ec636dac2d83399dec69b8fe5b39
pbypby <panboyang@126.com>
2015-05-03 09:28:51 +0800

    fisrt submission

---
+++ CompressiveTracker.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <vector>
+
+using std::vector;
+using namespace cv;
+//---------------------------------------------------
+class CompressiveTracker
+{
+public:
+	CompressiveTracker(void);
+	~CompressiveTracker(void);
+
+private:
+	int featureMinNumRect;
+	int featureMaxNumRect;
+	int featureNum;
+	vector<vector<Rect>> features;
+	vector<vector<float>> featuresWeight;
+	int rOuterPositive;
+	vector<Rect> samplePositiveBox;
+	vector<Rect> sampleNegativeBox;
+	int rSearchWindow;
+	Mat imageIntegral;
+	Mat samplePositiveFeatureValue;
+	Mat sampleNegativeFeatureValue;
+	vector<float> muPositive;
+	vector<float> sigmaPositive;
+	vector<float> muNegative;
+	vector<float> sigmaNegative;
+	float learnRate;
+	vector<Rect> detectBox;
+	Mat detectFeatureValue;
+	RNG rng;
+
+private:
+	void HaarFeature(Rect& _objectBox, int _numFeature);
+	void sampleRect(Mat& _image, Rect& _objectBox, float _rInner, float _rOuter, int _maxSampleNum, vector<Rect>& _sampleBox);
+	void sampleRect(Mat& _image, Rect& _objectBox, float _srw,    vector<Rect>& _sampleBox);
+	void getFeatureValue(Mat& _imageIntegral, vector<Rect>& _sampleBox, Mat& _sampleFeatureValue);
+	void classifierUpdate(Mat& _sampleFeatureValue, vector<float>& _mu, vector<float>& _sigma, float _learnRate);
+	void radioClassifier(vector<float>& _muPos, vector<float>& _sigmaPos, vector<float>& _muNeg, vector<float>& _sigmaNeg,
+						Mat& _sampleFeatureValue, float& _radioMax, int& _radioMaxIndex);
+public:
+	void processFrame(Mat& _frame, Rect& _objectBox);
+	void init(Mat& _frame, Rect& _objectBox);
+};
